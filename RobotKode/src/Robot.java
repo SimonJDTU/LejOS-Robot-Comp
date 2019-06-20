4b4b785a07521be8efa02e46e260e84230d50ee6
Daniel Greibe <Danielgreibe@gmail.com>
2019-06-20 22:47:55 +0200

    Added Javadocumentation.

--- RobotKode/src/Robot.java
+++ RobotKode/src/Robot.java
@@ -10,32 +10,12 @@ Motor D er robotten to hjul til at indsamle og udskyde bolde.
  */
 public class Robot
 {
-    /**
-     * Makes the robot move forward using the motors at input A and B
-     * @param speed The speed at which the two motors move
-     */
-    public void MoveForwardAB(int speed)
-    {
-        Motor.A.setSpeed(speed);
-        Motor.B.setSpeed(speed);
-
-        Motor.A.forward();
-        Motor.B.forward();
-    }
 
     /**
-     * Moves the robot backwards using the motors at input A and B
-     * @param speed The speed at which the two motors move
+     * Moves the robot a given distance forward.
+     * The method is blocking and will return once the robot have moved the given distance.
+     * @param distance The amount of cm the robot should move.
      */
-    public void MoveBackwardAB(int speed)
-    {
-        Motor.A.setSpeed(speed);
-        Motor.B.setSpeed(speed);
-
-        Motor.A.backward();
-        Motor.B.backward();
-    }
-
     public void MoveDistanceForwardAB(int distance)
     {
         Motor.A.setSpeed(600);
@@ -46,6 +26,12 @@ public class Robot
         Motor.B.rotate(rotation, false);
     }
 
+    /**
+     * Moves the robot a given distance forward with a given motor speed
+     * The method is blocking and will return once the robot have moved the given distance.
+     * @param distance The amount of cm the robot should move.
+     * @param speed The speed at which the motors should run at.
+     */
     public void MoveDistanceForwardAB(int distance, int speed)
     {
         Motor.A.setSpeed(speed);
@@ -55,6 +41,11 @@ public class Robot
         Motor.B.rotate(rotation, false);
     }
 
+    /**
+     * Moves the robot a given distance backwards.
+     * The method is blocking and will return once the robot have moved the given distance.
+     * @param distance The amount of cm the robot should move.
+     */
     public void MoveDistanceBackwardsAB(int distance)
     {
         Motor.A.setSpeed(600);
@@ -65,9 +56,8 @@ public class Robot
     }
 
     /**
-     * Turns the robot clockwise using the motors at input A as the left motor and and B as the right motor.
-     * The robot turns for a given duration in ms.
-     * @param degrees The amount of degrees the robot should turn clockwise.
+     * Turns the robot a given number of degrees clockwise.
+     * @param degrees The amount of degrees the robot should turn
      */
     public void TurnClockwiseAB(int degrees)
     {
@@ -80,32 +70,39 @@ public class Robot
         Motor.B.rotate(angle, false);
     }
 
+    /**
+     * Turns the robot a given number of degrees clockwise.
+     * @param degrees The amount of degrees the robot turn
+     * @param speed The speed the motors turn at
+     */
     public void TurnClockwiseAB(int degrees, int speed)
     {
 
         Motor.A.setSpeed(speed);
         Motor.B.setSpeed(speed);
         int  angle = (int) (degrees * 5.05);
-        //System.out.println("Angle is " + angle);
         Motor.A.rotate(-angle, true);
         Motor.B.rotate(angle, false);
     }
 
+    /**
+     * Turns the robot a given number of degrees counterclockwise.
+     * @param degrees The amount of degrees the robot should turn
+     * @param speed The speed the motors turn at
+     */
     public void TurnCounterclockwiseAB(int degrees, int speed)
     {
         Motor.A.setSpeed(speed);
         Motor.B.setSpeed(speed);
         System.out.println("Turned " + degrees + " degrees");
         int  angle = (int) (degrees * 5.077);
-        //System.out.println("Angle is " + angle);
         Motor.B.rotate(-angle, true);
         Motor.A.rotate(angle, false);
     }
 
     /**
-     * Turns the robot clockwise using the motors at input A as the left motor and and B as the right motor.
-     * The robot turns for a given duration in ms.
-     * @param degrees The amount of degrees the robot should turn counterclockwise
+     * Turns the robot a given number of degrees counterclockwise.
+     * @param degrees The amount of degrees the robot should turn
      */
     public void TurnCounterclockwiseAB(int degrees)
     {
@@ -118,31 +115,8 @@ public class Robot
         Motor.A.rotate(angle, false);
     }
 
-    public void TurnClockwiseAB()
-    {
-        Motor.A.setSpeed(400);
-        Motor.B.setSpeed(400);
-        Motor.A.forward();
-        Motor.B.backward();
-    }
-
-    public void TurnCounterclockwiseAB()
-    {
-        Motor.A.setSpeed(400);
-        Motor.B.setSpeed(400);
-        Motor.A.backward();
-        Motor.B.forward();
-    }
-
-    public void StopTurningAB()
-    {
-        Motor.A.stop(true);
-        Motor.B.stop(true);
-    }
-
-
     /**
-     *
+     * Starts the motor D at max speed and turns them to run backwards to capture balls.
      */
     public void CaptureBallsD()
     {
@@ -150,41 +124,43 @@ public class Robot
         Motor.D.backward();
     }
 
+    /**
+     * Starts the motor C at max speed moving the plate from the front of the robot to the back
+     * making space for balls.
+     * @param amount The amount of balls you make space for. Should always be 3.
+     */
     public void StoreBallsC(int amount)
     {
         Motor.C.setSpeed(Motor.C.getMaxSpeed());
         Motor.C.rotate(-amount*4300,false);
     }
+
+    /**
+     * Starts the motor C at max speed moving the plate from the back of the robot to the front
+     * shooting out balls out of the robot.
+     * @param amount The amount of balls you shoot out. Should always be 3.
+     */
     public void ThrowBallsC(int amount)
     {
         Motor.C.setSpeed(Motor.C.getMaxSpeed());
         Motor.C.rotate(amount*4300, true);
     }
 
+    /**
+     * Starts the motor D at max speed and turns it forward to release balls.
+     */
     public void ReleaseBallsD()
     {
         Motor.D.setSpeed(Motor.D.getMaxSpeed());
         Motor.D.forward();
     }
 
-    public void StopReleaseBalls()
-    {
-        Motor.D.stop();
-    }
-
-    public void StopRobotAB()
-    {
-        Motor.A.stop(true);
-        Motor.B.stop(true);
-    }
+    /**
+     * Stops the motor D which Stops the front wheels from moving
+     */
     public void StopRobotD()
     {
         Motor.D.stop(true);
     }
 
-    public void playSound()
-    {
-        Sound.setVolume(100);
-        Sound.beepSequenceUp();
-    }
 }
