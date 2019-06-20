55b766d98782f1ba701d9a5a4664576d6c342d09
SimonJDTU <SimonJDTU@gmail.com>
2019-06-20 21:15:54 +0200

    IComputerVision now correctly implemented in MovementManager and hsa correct public methods from ComputerVision.

--- src/IComputerVision.java
+++ src/IComputerVision.java
@@ -7,5 +7,9 @@ public interface IComputerVision {
     void run();
     ArrayList<Point> getRobotLocation();
     ArrayList<Point> getBallsLocation();
+    boolean cleanPath(Point startPoint, Point endPoint);
+    Point ballsCloseToEdge(Point currentBall);
+    boolean insideCircle(Point goal);
+    public Point circleRotation(Point goal);
 
 }
